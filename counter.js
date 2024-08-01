c041cb98c991da9e6be2470c4abf18b24565c3c5
ngaga <nicolasgms6@gmail.com>
2024-08-01 14:22:22 +0200

    fix folders pathes

--- counter.js
+++
@@ -1,7 +0,0 @@
-
-  function incrementCounter() {
-    const counterElement = document.getElementById('counter');
-    let counterValue = parseInt(counterElement.textContent);
-    counterValue += 2;
-    counterElement.textContent = counterValue.toString();
-  }
